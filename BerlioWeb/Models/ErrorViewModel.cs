db1443d59a0253c2f341b8eb32bbeaee7cd0e844
artplay2006 <artemlavdar@gmail.com>
2025-02-16 10:05:38 +0300

    Добавьте файлы проекта.

---
+++ BerlioWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BerlioWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
