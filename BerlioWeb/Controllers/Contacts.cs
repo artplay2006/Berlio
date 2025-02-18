2b9e20cf2cf01280301a8aa8b8f990003778daa9
Artem <artemlavdar@gmail.com>
2025-02-19 00:22:21 +0300

    Contacts and About

---
+++ BerlioWeb/Controllers/Contacts.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BerlioWeb.Controllers
+{
+    public class Contacts : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
