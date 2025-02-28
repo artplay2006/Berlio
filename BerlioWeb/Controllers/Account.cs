95e4ad8c766124dfc89c4eb0b017fc3d84150d60
Artem <artemlavdar@gmail.com>
2025-02-28 16:03:33 +0300

    Reg and Auth
    not completely

---
+++ BerlioWeb/Controllers/Account.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BerlioWeb.Controllers
+{
+    public class Account : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Registration()
+        {
+            return View();
+        }
+        public IActionResult Authorization()
+        {
+            return View();
+        }
+    }
+}
